f7d56ead3057f51a9712b33a0d3bdda9efa59443
Nikolay <mushk111@yandex.ru>
2023-10-21 16:19:36 +0800

    Fixed: styles in SearchForm component

--- src/components/search/SearchForm.tsx
+++ src/components/search/SearchForm.tsx
@@ -3,7 +3,7 @@ const SearchForm = () => {
     <form action="/search" className="z-20 w-full flex justify-center">
       <input
         name="search"
-        className="bg-white w-1/2 max-sm:w-72 p-5 pl-6 rounded-tl-full rounded-bl-full text-black text-lg  placeholder-gray-900 outline-none max-md:p-3 max-md:text-sm"
+        className="bg-white w-1/2 max-sm:w-72 p-5 pl-6 rounded-tl-full rounded-bl-full text-black text-lg  placeholder-gray-900 outline-none  max-md:text-sm"
         placeholder="Enter a movie or TV show title..."
       />
       <button className="w-1/6 max-sm:w-24 bg-gradient-main rounded-tr-full rounded-br-full text-lg max-md:text-sm">
